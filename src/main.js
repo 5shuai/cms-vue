cd42f800e0cfade25756c514d857dec68f1a0226
武帅帅 <wuss@2345.com>
2019-10-09 16:21:51 +0800

    add code ~

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import app from "./app.vue";
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
