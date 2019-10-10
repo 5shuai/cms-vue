85899552512e55d17222fbf3efcc36f1a651b27b
武帅帅 <wuss@2345.com>
2019-10-10 14:19:35 +0800

    add code ~

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import VueRouter from 'vue-router';
+import router from "./router.js";
+import Axios from "axios";
 
+Vue.use(VueRouter)
 Vue.use(MintUI)
+Vue.prototype.$http = Axios
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router
 })
--- src/router.js
+++ src/router.js
@@ -1,9 +1,19 @@
-import VueRouter from "vue-router";
+import VueRouter from 'vue-router';
+import HomeContainer from "./components/tabbar/HomeContainer.vue";
+import MemberContainer from "./components/tabbar/MemberContainer.vue";
+import SearchContainer from "./components/tabbar/SearchContainer.vue";
+import ShopCarContainer from "./components/tabbar/ShopCarContainer.vue";
 
-// var router = new VueRouter({
-//   routers:[
-//     {
-//       path:'#tabbar',component:
-//     }
-//   ]
-// })
+var router = new VueRouter({
+  routes: [
+    {path: '/', redirect: '/home'},
+    {path: '/home', component: HomeContainer},
+    {path: '/member', component: MemberContainer},
+    {path: '/shopcar', component: ShopCarContainer},
+    {path: '/search', component: SearchContainer}
+  ],
+  linkActiveClass: 'mui-active'
+})
+
+
+export default router
